7bc45539b47f4249bfd34bc62ffa60a62ba85ec9
AmulyaX <amulya.b520@gmail.com>
2020-03-23 19:39:57 +0530

    lavender: Adapt for Potato
    Signed-off-by: AmulyaX <amulya.b520@gmail.com>

--- device.mk
+++ device.mk
@@ -52,8 +52,7 @@ PRODUCT_BOOT_JARS += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(DEVICE_PATH)/overlay \
-    $(DEVICE_PATH)/overlay-lineage
+    $(DEVICE_PATH)/overlay
 
 PRODUCT_PACKAGES += \
     NoCutoutOverlay
